9ebc2206021f3d73adfe39c122981251cf4624c7
Mohamed Ali Sammoud <Mohamed.Ali.Sammoud@gmail.com>
2021-12-26 21:47:38 +0100

    Create Component Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+//import Particles from "react-particles-js";
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 function App() {
   const [lang, setLang] = useState(true);
   return (
     <div className="App">
+      {/* <Particles
+      className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              }
+            },
+            shape :{
+              type:"circle",
+              stroke: {
+                width:6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      /> */}
       <NavBar setLang={setLang} lang={lang} />
+      <Header lang={lang} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = ({ lang }) => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1>
+          {lang
+            ? "Web development and Mobile development"
+            : "Développement Web et Développement Mobile"}
+        </h1>
+        <Typed
+          className="typedText"
+          strings={["Web Design", "Web Development", "Mobile Development"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
